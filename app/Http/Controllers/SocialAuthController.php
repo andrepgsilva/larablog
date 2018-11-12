5075fbd24e1b3527f4d09e4e4bf0b32b09ab6aa5
Andre Luiz Pergentino da Silva <andrepgsilva@outlook.com>
2018-11-12 01:12:02 +0000

    Split createOrGetUser in three methods: process user, verifyUser, createUser

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -22,27 +22,35 @@ class SocialAuthController extends Controller
         return redirect('/');
     }
 
-    private function validateUser($providerName, $providerUser)
+    //Execute Create/Get user process
+    private function processUser(Provider $provider)
     {
-        return User::whereEmail($providerUser->getEmail())->first();
-    }
-
-    private function createOrGetUser(Provider $provider)
-    {
-        //Get social provider user
         $providerUser = $provider->user();
         $providerName = class_basename($provider);
         $user = $this->verifyUser($providerName, $providerUser);
+
         if (empty($user)) {
-            $user = User::create([
-                'username' => $providerUser->getName(),
-                'email' => $providerUser->getEmail(),
-                'email_verified_at' => date("Y-m-d H:i:s"),
-                'provider_id' => $providerUser->getId(),
-                'provider' => $providerName
-            ]);
+            return $this->createUser($providerUser, $providerName);
         }
+        
         return $user;
+    }
+
+    // Verify user existence
+    private function verifyUser($providerName, $providerUser)
+    {
+        return User::whereEmail($providerUser->getEmail())->first();
+    }
+ 
+    private function createUser($providerUser, $providerName)
+    {
+        return User::create([
+            'username' => $providerUser->getName(),
+            'email' => $providerUser->getEmail(),
+            'email_verified_at' => date("Y-m-d H:i:s"),
+            'provider_id' => $providerUser->getId(),
+            'provider' => $providerName
+        ]);
     }
 
     public function logout()
