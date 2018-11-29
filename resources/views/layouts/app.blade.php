ff6b75e35d39bce5f4f0906f044f583ba519dff4
Andre Luiz Pergentino da Silva <andrepgsilva@outlook.com>
2018-11-29 02:57:48 +0000

    fix csrf axios, sourceMaps. Validation form errors moved to a single file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
