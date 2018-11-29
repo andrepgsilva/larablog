abf0c5f3abe637e0354f75aff7e53ab638a84f16
Andre Luiz Pergentino da Silva <andrepgsilva@outlook.com>
2018-11-29 19:19:46 +0000

    Added component row for posts table on home page

---
+++ resources/views/layouts/components/post-row-table.blade.php
@@ -0,0 +1,17 @@
+<tr>
+    <td>{{ $title }}</td>
+    <td>{{ $article }}</td>
+    <td>{{ $tags }}</td>
+    <td>{{ $created_at }}</td>
+    <td>{{ $updated_at }}</td>
+    <td class="text-center">
+        <a href="" class="text-danger">
+            <i class="fa fa-trash"></i>
+        </a>
+    </td>
+    <td class="text-center">
+        <a href="" class="text-primary">
+            <i class="fa fa-edit"></i>
+        </a>
+    </td>
+</tr>
