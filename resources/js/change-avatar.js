21bbd907fbf651fa28d4922c7e1f1d73062854e8
Andre Luiz Pergentino da Silva <andrepgsilva@outlook.com>
2018-12-03 00:53:42 +0000

    Update profile with avatar

--- resources/js/change-avatar.js
+++ resources/js/change-avatar.js
@@ -1,9 +1,13 @@
-let avatar = document.getElementById('profile-avatar');
-let fileInput = document.getElementsByName('avatar')[0];
+let avatar;
+let fileInput;
 
-fileInput.addEventListener('change', () => {
-    avatar.src = window.URL.createObjectURL(fileInput.files[0]);
-    avatar.onload = function() {
-        window.URL.revokeObjectURL(avatar.src);
+if (avatar = document.getElementById('profile-avatar')) {
+    if (fileInput = document.getElementsByName('avatar')[0]) {
+        fileInput.addEventListener('change', () => {
+            avatar.src = window.URL.createObjectURL(fileInput.files[0]);
+            avatar.onload = function() {
+                window.URL.revokeObjectURL(avatar.src);
+            }
+        });
     }
-});
+}
